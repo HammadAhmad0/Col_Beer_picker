576a8f8ceefc78da79b96797f9ff91e9c2df70b2
Ha166141 <Ha166141@gmail.com>
2023-01-06 20:42:19 +0530

    1st Commit

---
+++ app/src/main/java/com/example/fstart/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.fstart;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void onClickFindBeer(View view){
+        BeerExpert expert = new BeerExpert();
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Get recommendations from the BeerExpert class
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+//Display the beers
+        brands.setText(brandsFormatted);
+
+    }
+}
